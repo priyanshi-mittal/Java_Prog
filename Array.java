53398d1cdbea62c3dce0c749fe40380e71c53d05
priyanshi-mittal <80943199+priyanshi-mittal@users.noreply.github.com>
2021-12-30 20:41:49 +0530

    Array
    Array prog

---
+++ Array.java
@@ -0,0 +1,16 @@
+class array
+{
+	public static void main(String args[])
+	{
+		int ar[] = new int[5];
+		ar[0] = 10;
+		ar[1] = 12;
+		ar[2] = 14;
+		ar[3] = 16;
+		ar[4] = 18;
+		System.out.println(".....array elements.....");
+		for(int i = 0; i < 5; i++){
+		System.out.println(ar[i]);
+		}
+	}
+}
