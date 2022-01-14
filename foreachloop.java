8c6242261fb7630c7df2da904f4840d0df6a4428
priyanshi-mittal <80943199+priyanshi-mittal@users.noreply.github.com>
2022-01-14 21:59:04 +0530

    All elements of numeric array by foreach loop

---
+++ foreachloop.java
@@ -0,0 +1,14 @@
+//Q - print all elements of numeric array by using foreach loop.
+
+class foreachexample
+{
+	public static void main(String args[])
+	{
+		int ar[] = {10,20,30,40,50,60,70,80,90,100};
+		//foreach loop
+		for(int var : ar)
+		{
+			System.out.println(var);
+		}
+	}
+}
