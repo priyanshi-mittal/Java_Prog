3ed44fc58bda6d3c73aadc112e3f6c278ae7af32
priyanshi-mittal <80943199+priyanshi-mittal@users.noreply.github.com>
2022-01-26 23:21:29 +0530

    StringBuffer

---
+++ stringbufferexample.java
@@ -0,0 +1,22 @@
+class stringbufferexample
+{
+	public static void main(String args[])	
+	{
+		StringBuffer sb1 = new StringBuffer("Java");
+		//StringBuffer sb2 = new StringBuffer("Programming");
+		
+		System.out.println(sb1);
+		//System.out.println(sb2);
+
+		System.out.println("\n\n...................\n\n");
+	
+		//sb1.append(sb 2);
+		sb1.reverse();
+		System.out.println(sb1);
+	}
+}
+
+
+
+		
+		
