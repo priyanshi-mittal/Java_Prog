57e67836b85bdea24261aef447f3dd256c05f119
priyanshi-mittal <80943199+priyanshi-mittal@users.noreply.github.com>
2022-01-30 21:44:20 +0530

    Password example

---
+++ password.java
@@ -0,0 +1,20 @@
+//write a program if string legnth is 8-15 password valid else if less then8-15 password invalid.
+
+class password
+{
+	public static void main(String args[])
+	{
+		String str1 = new String();
+		System.out.println("Enter your password");
+		int len = str1.length();
+	}	
+		if(len<=15)
+		{
+			System.out.println("valid");
+		}
+		else
+		{
+			System.out.println("Invalid");
+		}
+}
+		
