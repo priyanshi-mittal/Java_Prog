e2ff4811d89eb0cc11a1b89beca44963b0a6c0d1
priyanshi-mittal <80943199+priyanshi-mittal@users.noreply.github.com>
2022-02-01 22:35:56 +0530

    String example2

---
+++ Stringexample (2).java
@@ -0,0 +1,12 @@
+class stringexample
+{
+	public static void main(String args[])
+	{
+		String str1= "Hello We are LeaRNinG JAvA";
+		System.out.pritnln(str1);	
+		System.out.println("\n\n.........................\n\n");
+		String res = str1.toUpperCase();
+		System.out.pritnln(res);
+	}
+}
+
