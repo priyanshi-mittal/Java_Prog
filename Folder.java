900974e40f14ca30d9326caf8af4a2f0aeeb7fd1
priyanshi-mittal <80943199+priyanshi-mittal@users.noreply.github.com>
2022-02-07 23:22:19 +0530

    foreachloop

---
+++ Folder.java
@@ -0,0 +1,15 @@
+class foreachexample
+{
+	public static void main(String args[])
+	{
+		String days[] = {"Sun" , "Mon" , "Tues" , "Wed" , "Thu" , "Fri" , "Sat"};
+		for(String var : days)
+		{		
+			System.out.println(var);
+		}
+	}
+}
+
+
+		
+			
